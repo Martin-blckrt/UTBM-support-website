b6ef1679aa99b29a1e694901a02f5f64b687d78e
alexandre <alexandre.des3os@gmail.com>
2021-03-22 17:55:30 +0100

    Initial commit from gatsby

---
+++ gatsby-config.js
@@ -0,0 +1,15 @@
+module.exports = {
+  siteMetadata: {
+    title: "UTBM support",
+  },
+  plugins: [
+    "gatsby-plugin-sass",
+    "gatsby-plugin-sitemap",
+    {
+      resolve: "gatsby-plugin-manifest",
+      options: {
+        icon: "src/images/icon.png",
+      },
+    },
+  ],
+};
