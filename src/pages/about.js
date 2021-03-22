f19705588b4dae73960fa8e3f4d7e029d032fc49
alexandre <alexandre.des3os@gmail.com>
2021-03-22 21:13:44 +0100

    update init

--- src/pages/about.js
+++ src/pages/about.js
@@ -1,10 +1,14 @@
 import React from "react"
+import Layout from "../components/layout";
 
 export default function About() {
     return (
-        <div style={{ color: `teal` }}>
-            <h1>About Gatsby</h1>
-            <p>Such wow. Very React.</p>
-        </div>
+        <Layout>
+            <div style={{ color: `teal` }}>
+                <h1>About Gatsby</h1>
+                <p>Such wow. Very React.</p>
+            </div>
+        </Layout>
+
     )
 }
