9ddbc5680951b82b7effad9a484d6e8069fa373e
Martin Blanckaert <martinblanckaert@yahoo.fr>
2021-04-08 12:34:53 +0200

    articleservice

--- src/components/articles.js
+++ src/components/articles.js
@@ -1,20 +1,82 @@
 import React, {Component} from "react";
 import Article from "../pages/article";
+import ArticleService from '../services/ArticleService'
 
-export default class ArticleComponent extends Component {
+export default class ListArticleComponent extends Component {
     constructor(props) {
         super(props);
         //https://www.javaguides.net/2020/08/reactjs-axios-get-post-put-and-delete-example-tutorial.html
         //TODO. finir en se basant la dessus et en mettant les infos qu'il faut au bon endroit.
 
         this.state = {
-
+                articles: []
         }
+        this.addArticle = this.addArticle.bind(this);
+        this.editArticle = this.editArticle.bind(this);
+        this.deleteArticle = this.deleteArticle.bind(this);
     }
+
+    deleteArticle(id){
+        ArticleService.deleteArticle(id).then( res => {
+            this.setState({articles: this.state.articles.filter(articles => article.id !== id)});
+        });
+    }
+
+    viewArticle(id){
+        this.props.history.push(`/view-article/${id}`);
+    }
+
+    editArticle(id){
+        this.props.history.push(`/add-article/${id}`);
+    }
+
     componentDidMount(){
-        Article.getArticle().then((res) => {
+        ArticleService.getArticle().then((res) => {
             this.setState({ employees: res.data}); //TODO. change this point
         });
     }
 
+    addArticle(){
+        this.props.history.push(`/add-article/_add`);
+    }
+
+    render(){
+        return (
+            <div>
+                <h2 className="text-center">Articles List</h2>
+                <div className="row">
+                    <button className="btn btn-primary" onClick={this.addArticle}>AddArticle</button>
+                </div>
+                <br></br>
+                <div className="row">
+                    <table className="table table-striped table-bordered">
+                        <thead>
+                        <tr>
+                            <th>Article Title</th>
+                        </tr>
+                        </thead>
+                        <tbody>
+                            {
+                                this.state.articles.map(
+                                    article =>
+                                        <tr key = {article.id}>
+                                            <td>{article.title}</td>
+                                            <td>
+                                                <button onClick={ () => this.editArticle(article.id)} className="btn btn-info">Update</button>
+                                                <button style={{marginLeft: "10px"}} onClick{ () => this.deleteArticle(article.id)} className="btn btn-danger">Delete </button>
+                                                <button style={{marginLeft: "10px"}} onClick{ () => this.viewArticle(article.id)} className="btn btn-info">View </button>
+                                            </td>
+                                        </tr>
+                                )
+                            }
+                        </tbody>
+                    </table>
+
+                </div>
+
+            </div>
+
+
+        )
+    }
 }
--- src/services/articleService.js
+++ src/services/articleService.js
@@ -1,6 +1,6 @@
 import axios from 'axios';
 
-const ARTICLE_API_BASE_URL = "http://localhost:8080/api/v1/articles";
+const ARTICLE_API_BASE_URL = "http://localhost:8000/api/v1/articles";
 
 class ArticleService {
 
@@ -12,8 +12,16 @@ class ArticleService {
         return axios.post(ARTICLE_API_BASE_URL, article);
     }
 
+    getArticleByID(articleID){
+        return axios.get(ARTICLE_API_BASE_URL + '/' + articleID);
+    }
+
     updateArticle(article, articleID){
         return axios.put(ARTICLE_API_BASE_URL + '/' + article, articleID );
+    }
+
+    deleteArticle(articleID){
+        return axios.delete(ARTICLE_API_BASE_URL + '/' + articleID);
     }
 
 }
