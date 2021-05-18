d4acf5ad93c37477030daea17b4bb03c8c0bff4b
Martinoux <martinblanckaert@yhaoo.fr>
2021-05-18 14:56:11 +0200

    working on search bar

--- server/routes/api/searchBar.js
+++ server/routes/api/searchBar.js
@@ -4,9 +4,9 @@ let router = express.Router();
 
 const db_manager = new DBmanager();
 
-router.get('/', async (req, res) => {
+router.get('/category', async (req, res) => {
 
-    const query = `SELECT c.id as idCategory, c.name, a.id as idArticle, a.articleTitle
+    const query = `SELECT DISTINCT c.id as idCategory, c.name
                    FROM T_category c
                             INNER JOIN T_article a ON c.id = a.idCategory
                    ORDER BY c.id`;
@@ -16,5 +16,14 @@ router.get('/', async (req, res) => {
         .catch(err => console.error(err));
 });
 
+router.get('/article', async (req, res) => {
+
+        const query = `SELECT a.id as idArticle, a.articleTitle
+                       FROM T_article a`;
+
+    await db_manager.getDataDB(query)
+        .then(results_db => res.send(results_db))
+        .catch(err => console.error(err));
+});
 
 module.exports = router
--- src/components/searchBar.jsx
+++ src/components/searchBar.jsx
@@ -1,52 +1,46 @@
-import {useEffect, useState} from "react";
+import React, {useEffect, useState} from "react";
 import {Autocomplete} from "@material-ui/lab";
 import {TextField} from "@material-ui/core";
+import axios from "axios";
+import {Link} from "gatsby";
 
 
-export default function SearchBar(props) {
-
-    let listCategories = [];
-    let listArticles = [];
-    let indexCategories = [];
-    let indexArticles = [];
+export default function SearchBar() {
 
     let [searchBarInfo, setSearchBarInfo] = useState([])
+    let [searchBarCategoryInfo, setSearchBarCategoryInfo] = useState([])
+    let [searchBarArticleInfo, setSearchBarArticleInfo] = useState([])
 
-    const fetchSearchBarInfo = () => {
-        fetch("api/searchBar").then((response) => {
-            return response.json()
-        }).then((res) => {
-            for (let i = 0; i < res.length; i++) {
+    useEffect(() => {
+        const fetchSearchBarCategory = async (url) => {
+            const category = await axios.get(url)
+            setSearchBarCategoryInfo(category.data);
+        };
+        fetchSearchBarCategory("/api/searchBar/category")
 
-                listCategories[i] = res[i].name
-                listArticles[i] = res[i].articleTitle
-                indexCategories[i] = res[i].idCategory
-                indexArticles[i] = res[i].idArticle
-                /*
-                searchBarInfo.push(res.data[i].name)
-                searchBarInfo.push(res.data[i].articleTitle)*/
-            }
-            searchBarInfo = []
-            let options = listCategories.concat(listArticles)
-            options = uniq(options)
+        const fetchSearchBarArticle = async (url) => {
+            const article = await axios.get(url)
+            setSearchBarArticleInfo(article.data);
+        };
+        fetchSearchBarArticle("/api/searchBar/article")
+    }, []);
 
-            indexCategories = uniq(indexCategories)
-            indexArticles = uniq(indexArticles)
-            let optionsIndex = indexCategories.concat(indexArticles)
-
-            for (let i = 0; i < options.length; i++) {
-                searchBarInfo.push(options[i])
-            }
-
-            setSearchBarInfo(searchBarInfo)
-            console.log(searchBarInfo)
-        })
+    const getSearchBarInfo = () => {
+        searchBarInfo = []
+        for (let i = 0; i < searchBarCategoryInfo.length; i++) {
+            searchBarInfo.push(searchBarCategoryInfo[i].name)
+        }
+        for (let i = 0; i < searchBarArticleInfo.length; i++) {
+            searchBarInfo.push(searchBarArticleInfo[i].articleTitle)
+        }
+        console.log(searchBarInfo)
+        setSearchBarInfo(searchBarInfo)
     }
 
     return (
         <div>
             <Autocomplete
-                style={{ width: 500 }}
+                style={{width: 500}}
                 freeSolo
                 autoComplete
                 autoHighlight
@@ -54,10 +48,21 @@ export default function SearchBar(props) {
                 getOptionLabel={(option) => option.toString()}
                 renderInput={(params) => (
                     <TextField {...params}
-                               onChange={fetchSearchBarInfo}
+                               onChange={getSearchBarInfo}
                                variant="outlined"
                                label="Recherchez ce que vous cherchez ici"
                     />
+                )}
+                renderOption={(option) => (
+                    <span style={{cursor: "pointer"}}>
+                        {
+                            //TODO. checker si {option} est égal à une string de mes catégories, si oui alors c'est une catégorie si non alors c'ets un article
+                            //TODO. retouver l'index de cet article/catégorie et donc l'id associé dans l'index de l'autre liste
+                        }
+                        <Link to='/category/' state={{id:searchBarCategoryInfo[1].idCategory}}>
+                            {option}
+                        </Link>
+                    </span>
                 )}
             />
         </div>
@@ -66,7 +71,7 @@ export default function SearchBar(props) {
 
 function uniq(a) {
     let seen = {};
-    return a.filter(function(item) {
+    return a.filter(function (item) {
         return seen.hasOwnProperty(item) ? false : (seen[item] = true);
     });
 }
