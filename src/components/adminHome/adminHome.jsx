4670e71c44398b0cad4369e0d235a8d930f450d5
alexandre <alexandre.des3os@gmail.com>
2021-06-10 21:59:37 +0200

    start fixing adminHome.jsx style. - 1h30

--- src/components/adminHome/adminHome.jsx
+++ src/components/adminHome/adminHome.jsx
@@ -13,50 +13,42 @@ import ModifyArticleForm from "../Forms/ModifyArticleForm"
 export default function AdminHome() {
 
     return (
-        <div className={adminHomeStyle.bigContainer}>
-            <div className={adminHomeStyle.title}>
-                <h2>Créer</h2>
-            </div>
+        <div className={adminHomeStyle.adminHomeContainer}>
+            <div className={adminHomeStyle.createContainer}>
+                <div className={adminHomeStyle.title}>
+                    <h2>Créer</h2>
+                </div>
 
-            <div className={adminHomeStyle.fourDiv} id="newCategory">
-                <h3>Une nouvelle catégorie</h3>
-                <CreateCategoryForm/>
-            </div>
+                <div className={adminHomeStyle.fourDiv} id="newCategory">
+                    <h3>Une nouvelle catégorie</h3>
+                    <CreateCategoryForm/>
+                </div>
 
-            <div className="separateBlueLine">
-                <svg width="5" height="408" viewBox="0 0 5 408" fill="none" xmlns="http://www.w3.org/2000/svg">
-                    <line x1="2.5" y1="300" x2="2.5" stroke="#3F8BFF" strokeWidth="5"/>
-                </svg>
+                <div className={adminHomeStyle.fourDiv} id="newArticle">
+                    <h3>Un nouvel article</h3>
+                    <p id="textCreateNewArticle">
+                        Accédez à l'outil de création en cliquant sur le bouton suivant
+                    </p>
+                    <CreateButton buttonText="Créer" type="article"/>
+                </div>
             </div>
+            <div className={adminHomeStyle.modifyContainer}>
+                <div className={adminHomeStyle.fourDiv} id="modifyArticle">
+                    <h3>Un article existant</h3>
+                    <ModifyArticleForm/>
+                </div>
 
-            <div className={adminHomeStyle.fourDiv} id="newArticle">
-                <h3>Un nouvel article</h3>
-                <p id="textCreateNewArticle">
-                    Accédez à l'outil de création en cliquant sur le bouton suivant
-                </p>
-                <CreateButton buttonText="Créer" type="article"/>
+                <div className={adminHomeStyle.title}>
+                    <h2>Modifier</h2>
+                </div>
+                <div className={adminHomeStyle.fourDiv} id="modifyCategory">
+                    <h3>Une catégorie existante</h3>
+                    <ModifyCategoryForm/>
+                </div>
             </div>
-
-            <div className={adminHomeStyle.title}>
-                <h2>Modifier</h2>
-            </div>
-            <div className={adminHomeStyle.fourDiv} id="modifyCategory">
-                <h3>Une catégorie existante</h3>
-                <ModifyCategoryForm/>
-            </div>
-
-            <div className="separateBlueLine">
-                <svg width="5" height="408" viewBox="0 0 5 408" fill="none" xmlns="http://www.w3.org/2000/svg">
-                    <line x1="2.5" y1="300" x2="2.5" stroke="#3F8BFF" strokeWidth="5"/>
-                </svg>
-            </div>
-
-            <div className={adminHomeStyle.fourDiv} id="modifyArticle">
-                <h3>Un article existant</h3>
-                <ModifyArticleForm/>
-            </div>
-
         </div>
-    )
+
+
+)
 
 }
